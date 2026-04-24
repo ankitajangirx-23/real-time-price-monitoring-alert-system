8bcc7db3dd335bbc868ce85c81bdc3c316a9d650
unknown <ankitajangirx@gmail.com>
2026-04-24 13:00:30 +0530

    successfully uploaded

---
+++ interface.py
@@ -0,0 +1,14 @@
+import streamlit as st
+st.title("real time")
+
+st.header("daily price tracking")
+st.header("historical storage")
+st.header("price comparison")
+st.header("alert system for price charge")
+st.header("automated pipeline")
+
+st.markdown("### database connective")
+st.successive("proceed successfully")
+st.info("find the formal information")
+st.warning("you are in high risk scenario")
+st.error("account has been blocked for 24 hours")
