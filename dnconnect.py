592957c0c944eba368b897a0d39369cba9c6b703
ankitajangirx-23 <ankitajangirx@gmail.com>
2026-05-01 13:21:13 +0530

    Add files via upload

---
+++ dnconnect.py
@@ -0,0 +1,14 @@
+import mysql
+import mysql.connector as sql
+
+db=sql.connect(
+    host="localhost",
+    user="root",
+    password="12345",
+    use_pure = True
+)
+cr=db.cursor()
+cr.execute("show databases")
+for i in cr:
+    print(i)
+
